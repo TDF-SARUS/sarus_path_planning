17c5065b84d6fe2d9d7d41713bb49f3af502c672
jgsamartin <jgsamartin@gmail.com>
2021-04-07 19:13:39 +0200

    Inicio de la función polygon

--- sarus_path_planning/planner.py
+++ sarus_path_planning/planner.py
@@ -10,10 +10,32 @@ from cpp_algorithms import get_drone_map, get_random_coords
 from cpp_algorithms import get_all_area_maps, plot, imshow, imshow_scatter
 from cpp_algorithms.darp.darp_helpers import get_assigned_count
 from cpp_algorithms.coverage_path.pathing_helpers import has_isolated_areas
+from skimage.draw import polygon
+from 
 
 # inicializar nodo ros
 
+# Para el polígono
+def polygon (points):
+    # Nos dan cuatro puntos
+    points = np.array(points)
 
+    # Acotamos el cuadrado
+    north = np.amax(points[:,1])
+    south = np.amin(points[:,1])
+    east = np.amax(points[:,0])
+    west = np.amin(points[:,0])
+
+    # Dividimos en grupos de 10
+    tam = north - south
+    tam2 = round(tam/10)    # Número de índices del array_map
+    tam3 = tam/tam2         # Ancho de barrido que usaremos
+
+    x = 
+
+    polygon(y,x)
+
+    return tam3
 
 n = 3
 area_maps = get_all_area_maps("test_maps")
