a5986ea1507b223c701604eca6315e99db76c311
jdcdp <47483528+jdcdp@users.noreply.github.com>
2021-04-07 16:22:26 +0200

    Update planner.py

--- sarus_path_planning/planner.py
+++ sarus_path_planning/planner.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
 
 
 import numpy as np
@@ -13,8 +11,8 @@ from cpp_algorithms import get_all_area_maps, plot, imshow, imshow_scatter
 from cpp_algorithms.darp.darp_helpers import get_assigned_count
 from cpp_algorithms.coverage_path.pathing_helpers import has_isolated_areas
 
+# inicializar nodo ros
 
-# In[2]:
 
 
 n = 3
@@ -23,20 +21,19 @@ area_map = area_maps[0]
 start_points = get_random_coords(area_map, n)
 
 
-# In[3]:
-
-
 start_points = get_random_coords(area_map, n)
 A, losses = darp(300, area_map, start_points, pbar=True)
 drone_maps = [get_drone_map(A,i) for i in range(n)]
 
 
-
-
 coverage_paths = [bcd(drone_maps[i],start_points[i]) for i in range(n)]
 
+# meter topic salida a aerostack drone111/
+
+
+
+# sacar un msg/path para interfaz
 
 
 
 
-plt.show()
