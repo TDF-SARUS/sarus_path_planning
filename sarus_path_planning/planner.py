1f42065457c6f0b6856bcb37ffcac6dd173161ed
Ubuntu <ubuntu@rosmel.j.net>
2021-04-08 18:56:16 +0000

    Updated planner.py

--- sarus_path_planning/planner.py
+++ sarus_path_planning/planner.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python
-
+#
+#  ADD DESCRIPTION
+#
+#
+#
 
 
 import numpy as np
 import matplotlib.pyplot as plt
-from cpp_algorithms import dist_fill
+from skimage.draw import polygon
+
 from cpp_algorithms import darp, stc, bcd, wavefront
 from cpp_algorithms import get_drone_map, get_random_coords
 from cpp_algorithms import get_all_area_maps, plot, imshow, imshow_scatter
 from cpp_algorithms.darp.darp_helpers import get_assigned_count
 from cpp_algorithms.coverage_path.pathing_helpers import has_isolated_areas
-from skimage.draw import polygon
 
-# inicializar nodo ros
 
-# Función auxiliar para cambiar un número N de un intervalo (a0, b0) a otro (aF, bF)
+
 def cambiaIntervalo (N, a0, b0, aF, bF):
     return aF + (bF - aF)*(N - a0)/(b0 - a0)
 
-# Para el polígono
-def makePolygon (points):
-    # Nos dan cuatro puntos
+
+def gazebo2alg (point):
+    newPoint = []
+    for i in range(len(point)):
+        newPoint.append(cambiaIntervalo(point(i), -100, 100, 0, 31))
+    return newPoint
+
+def alg2gazebo (point): #####FIX
+    point=np.array(point)
+    newPoint = point.copy()
+    for i in range(len(point)):
+        for j in range(len(point[i])):
+            for k in range(point[i][j]):
+                newPoint[i][j][k]= cambiaIntervalo(point[i][j][k], 0, 31, -100, 100)
+    return newPoint
+
+
+def makePolygon (points, width=10):	#Polygon to bitmap with pixels of ~ 10x10 m
+
     points = np.array(points)
 
     # Acotamos el cuadrado
@@ -31,7 +50,7 @@ def makePolygon (points):
 
     # Dividimos en grupos de 10
     Ysize = north - south
-    Yindexes = round(Ysize/10)          # Número de índices del array_map
+    Yindexes = round(Ysize/width)          # Número de índices del array_map
     Ywidth = Ysize/Yindexes             # Ancho de barrido que usaremos
 
     Xsize = north - south
@@ -45,25 +64,109 @@ def makePolygon (points):
 
     return area_maps, Ywidth, Xwidth
 
-n = 3
+def initPublisher(droneNum):
+    # Define the name of the drone's topic
+    topic='/drone11'+str(droneNum)+'/motion_reference/pose'
+    # Define the publisher and type of message
+    pub = rospy.Publisher(topic, PoseStamped, queue_size=10)
+    return pub
+
+def publishTrajectory(pub, coordinates):
+    msg = PoseStamped()
+    # Also possible with numpy
+    #[x_ref, y_ref] = np.split(np.array(coordinates), 2, axis = 1)
+    # Separate the position in its coordinates
+    msg.pose.position.x=coordinates[0]
+    msg.pose.position.y=coordinates[1]
+    msg.pose.position.z=1.0 #coordinates[2]
+    # Publish the message (coordinates of the position of drone droneNum)
+    pub.publish(msg)
+
+
+
+def poly_cb(data):
+    global base_polygon
+    base_polygon=np.array(data)
+
+def n_cb(data):
+    global n
+    n=data
+
+
+
+########################
+### Program Start ######
+########################
+
+
+rospy.init_node('coords_node', anonymous=True) #Create node
+
+# Frequency of the sleep
+rate = rospy.Rate(0.2) #0.2 Hz -> 5s
+
+# Datos de Interfaz:
+
+n   #n de drones
+
+base_polygon = [] #poligono del mapa
+
+
+
+rospy.Subscriber('/interfaz/poligono', POLYGON_TYPE, poly_cb)
+rospy.Subscriber('/interfaz/poligono', int8, poly_cb)
+
+
 area_maps = get_all_area_maps("test_maps")
-area_map = area_maps[0]
-start_points = get_random_coords(area_map, n)
+area_map = area_maps[1]
 
+#area_map, Xwidth, Ywidth=makePolygon(pol) #Make bitmap from polygon
 
-start_points = get_random_coords(area_map, n)
-A, losses = darp(300, area_map, start_points, pbar=True)
-drone_maps = [get_drone_map(A,i) for i in range(n)]
+start_points = get_random_coords(area_map, n) # Random start coordinates
 
+A, losses = darp(300, area_map, start_points, pbar=True) # Area division algorithm
 
-coverage_paths = [bcd(drone_maps[i],start_points[i]) for i in range(n)]
+drone_maps = [get_drone_map(A,i) for i in range(n)] #assign a map for each drone
 
-# meter topic salida a aerostack drone111/
+coverage_paths = [bcd(drone_maps[i],start_points[i]) for i in range(n)]  #Calculate the routes for each drone
 
 
 
 # sacar un msg/path para interfaz
+########### FALTA POR HACER ##########
 
+
+
+#################################################
+### meter topic salida a aerostack drone111/#####
+#################################################
+
+coverage_paths_gazebo = alg2gazebo(coverage_paths)
+
+
+# Drone with the maximum number of positions: Each drone has a different path with different number of points
+maxPos=0
+for drone in range(len(coverage_path_gazebo)):        # Number of drones
+    numberPos = len(coverage_path_gazebo[drone])
+    if numberPos>maxPos:
+        maxPos=numberPos
+
+pubs = []
+for drone in range(n):        # Number of drones
+    pubs.append(startpublishers(drone+1)) #+1 because in Python first array is 0
+
+
+# Execute the function for every position and for all of the drones
+for pos in maxPos:                                # Maximum number of positions
+    for drone in range(len(coverage_path_gazebo)):        # Number of drones
+        # Try and except to deal with the problem of having different number of positions
+        try:
+            # Call the function publishTrajectory sending the number of the drone and the desired position
+            publishTrajectory(drone+1, coverage_path_gazebo[drone][pos])
+            print('Drone ',drone+1, ' = ', coverage_path_gazebo[drone][pos])
+        except:
+            print('Drone ',drone+1, ' has reached its final position')
+    print()
+    rate.sleep() # 5s
 
 
 
